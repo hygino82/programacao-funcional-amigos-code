9bd72158576db35e7b07e49e1b9cd9120320aa13
Adroaldo <hygino@msn.com>
2022-07-29 22:26:28 -0300

    Imperactive and Declarative Approach

---
+++ src/main/java/imperactive/Main.java
@@ -0,0 +1,61 @@
+package imperactive;
+
+import static imperactive.Main.Gender.FEMALE;
+import static imperactive.Main.Gender.MALE;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<Person> people = List.of(
+                new Person("Gorete", FEMALE),
+                new Person("Godofredo", MALE),
+                new Person("Tenorio", MALE),
+                new Person("Jupira", FEMALE),
+                new Person("Dilma", FEMALE)
+        );
+
+        //imperative approach
+        System.out.println("Imperative Approach");
+        List<Person> females = new ArrayList<>();
+
+        for (Person person : people) {
+            if (FEMALE.equals(person.gender)) {
+                females.add(person);
+            }
+        }
+
+        for (Person female : females) {
+            System.out.println(female);
+        }
+
+        //declarative approach
+        System.out.println("\nDeclarative Approach");
+        var females2 = people.stream()
+                .filter(person -> FEMALE.equals(person.gender))
+                .collect(Collectors.toList());
+        females2.forEach(System.out::println);
+    }
+
+    static class Person {
+
+        private final String name;
+        private final Gender gender;
+
+        public Person(String name, Gender gender) {
+            this.name = name;
+            this.gender = gender;
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" + "name=" + name + ", gender=" + gender + '}';
+        }
+    }
+
+    enum Gender {
+        MALE, FEMALE;
+    }
+}
